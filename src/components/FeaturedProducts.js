436a1075de985495947ca8a031d88e24e3db916c
tsukasa@live.ca <tsukasa@live.ca>
2020-02-19 17:22:08 -0500

    Make featured section dynamic

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -1,79 +1,46 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 
 import './FeaturedProducts.scss'
-
-import Varmint from '../images/Varmint.jpg'
-import Bolero from '../images/Bolero.jpg'
-import MegaDistortion from '../images/MegaDistortion.jpg'
+import ProductCard from './ProductCard'
 
 const FeaturedProducts = () => {
-
-    const data = [
-        {
-            name: "Varmint",
-            img: Varmint,
-            shortDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
-            // url:,
-            regPrice: 160,
-            promoPrice: 120,
-        },
-        {
-            name: "Bolero",
-            img: Bolero,
-            shortDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
-            // url:,
-            regPrice: 180,
-            promoPrice: null,
-        },
-        {
-            name: "Mega Distortion",
-            img: MegaDistortion,
-            shortDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
-            // url:,
-            regPrice: 200,
-            promoPrice: 120,
-        },
-    ]
-
+    const data = useStaticQuery(graphql`
+    query {
+      allContentfulProducts(limit: 3, filter: {featured: {eq: true}}) {
+        edges {
+          node {
+            id
+            productName
+            price
+            promoPrice
+            slug
+            description {
+              description
+            }
+            images {
+              fluid(maxWidth: 400, maxHeight: 400) {
+                ...GatsbyContentfulFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
     return (
         <section id="featured-products">
             <div className="featured-products-container">
                 <div className="">
                     <h2 className="">Featured Products</h2>
-                    <div className="product-wrapper columns">
-                        {data.map( product => (
-                            <article className="product column">
-                                <figure className="image is-square">
-                                    <img src={product.img} className=""/>
-                                </figure>
+                    <div className="products">
 
-                                { product.promoPrice === null ? (
-                                    <div className="price-tag">
-                                        <div className="price-wrapper">
-                                            <p className="price"> ${product.regPrice} </p>
-                                        </div>
-                                    </div>
-                                ):(
-                                    <div className="price-tag promo">
-                                        <div className="price-wrapper">
-                                            <p className="price"> ${product.regPrice} </p>
-                                            <p className="promo-price"> ${product.promoPrice} </p>
-                                        </div>
-                                    </div>
-                                )}
+                    {data.allContentfulProducts.edges.map(({ node }) => (
+                        <ProductCard productInfo={node} />
+                    ))}
 
-                                <div className="short-description">
-                                    <div className="short-description-wrapper">
-                                        <h3>{product.name}</h3>
-                                        <p>{product.shortDescription}</p>
-                                    </div>
-                                </div>
-                                <button className="view-details-btn">
-                                    View Details
-                                </button>
-                            </article>
-                        ))}
                     </div>
+
                 </div>
             </div>
         </section>
